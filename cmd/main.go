f39e092b0603a35dd2a90b55e7fb1bd82701e856
toonDev <nattawoot.suk@dappmaker.co.th>
2026-03-18 12:20:08 +0700

    fix: env

--- cmd/main.go
+++ cmd/main.go
@@ -14,9 +14,7 @@ import (
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	godotenv.Load()
 
 	db, err := config.ConnectDB()
 	if err != nil {
